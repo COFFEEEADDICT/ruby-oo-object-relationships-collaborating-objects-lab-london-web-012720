8188fa2bdc872248c94059477ab04f30bdcec9e6
Bryn <thenazboz@gmail.com>
2020-02-04 14:46:02 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer
+
+    attr_accessor :path
+
+    def initialize(list_of_filenames)
+        @path = list_of_filenames
+    end
+#################################################
+    def files 
+        file_names = Pathname(@path).glob("*.mp3")
+        file_names = file_names.map{|file_name|file_name.basename.to_s}
+    end
+
+    def import 
+        files.map{|file|Song.new_by_filename(file)}
+    end
+
+end
